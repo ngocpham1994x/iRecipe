1664242cf7309f6db0faabb0c90758c62bceef36
Yixi <wuy8@myumanitoba.ca>
2022-02-22 18:26:52 -0600

    Change the format of CSV File, separate Instructions to another csv

--- app/src/main/java/comp3350/iRecipe/GetRecipeFromCSV.java
+++ app/src/main/java/comp3350/iRecipe/GetRecipeFromCSV.java
@@ -13,13 +13,15 @@ public class GetRecipeFromCSV {
     static final int PREPTIME = 3;
     static final int COOKINGTIME = 4;
     static final int SERVING = 5;
-    static final int INSTRUCTION = 6;
 
     //Column number in Ingredients.csv
     static final int INGREDIENTS = 1;
 
     //Column number in keyIngredients.csv
     static final int KEYINGREDIENTS = 1;
+
+    //Column number in Instructions.csv
+    static final int INSTRUCTION = 1;
 
     public static ArrayList<Recipe> getRecipe(){
         ArrayList<Recipe> allRecipe = new ArrayList<>();
@@ -31,12 +33,25 @@ public class GetRecipeFromCSV {
             while( (line = recipeReader.readLine()) != null ){
                 String[] data = line.split(",");
                 String name = data[RECIPENAME];
+                String insturctions = "";
                 ArrayList<String> ingred = new ArrayList<>();
                 ArrayList<String> keyIngred = new ArrayList<>();
 
+                BufferedReader readInstruct = new BufferedReader(new FileReader("Instructions.csv"));
+                String instructLine = readInstruct.readLine();      //Skip first header line
+                while( (instructLine = readInstruct.readLine()) != null){
+
+                    String[] instructData = instructLine.split(",");
+                    //If the name matches, add to the Recipe object
+                    if(instructData[RECIPENAME].equals(name) ){
+                        insturctions += instructData[INSTRUCTION] + "\n";
+                    }
+                }
+                readInstruct.close();
+
                 Recipe recipe = new Recipe(data[RECIPENAME], data[CATEGORY], data[COOKINGLEVEL],
                         Integer.parseInt(data[PREPTIME]), Integer.parseInt(data[COOKINGTIME]),
-                        Integer.parseInt(data[SERVING]), ingred, keyIngred, data[INSTRUCTION]);
+                        Integer.parseInt(data[SERVING]), ingred, keyIngred, insturctions);
 
                 //Read ingredients and add to recipe
                 BufferedReader readIngred = new BufferedReader(new FileReader("Ingredients.csv"));
