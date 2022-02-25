c90b8babcedee9066f80716ab05bbe4ef808c679
NgocPham <ngoc.pham.nm@gmail.com>
2022-02-25 17:07:36 -0600

    Initialized view of main page

--- app/src/main/java/comp3350/iRecipe/MainActivity.java
+++ app/src/main/java/comp3350/iRecipe/MainActivity.java
@@ -1,6 +1,8 @@
 package comp3350.iRecipe;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,6 +10,8 @@ import android.view.View;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    RecyclerView recyclerView;
+    Adapter adapter;
 
     public static final String EXTRA_MESSAGE = "comp3350.iRecipe.MESSAGE";
 
@@ -15,16 +19,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        recyclerView = findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new GridLayoutManager(MainActivity.this, 2));
+        adapter = new Adapter(this);
+        recyclerView.setAdapter(adapter);
     }
 
 
     /** Called when the user taps the Send button */
-    public void sendMessage(View view) {
-        Intent intent = new Intent(this, DisplayMessageActivity.class);
-        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
-        String message = editText.getText().toString();
-        intent.putExtra(EXTRA_MESSAGE, message);
-        startActivity(intent);
-    }
+//    public void sendMessage(View view) {
+//        Intent intent = new Intent(this, DisplayMessageActivity.class);
+//        EditText editText = (EditText) findViewById(R.id.editTextTextPersonName);
+//        String message = editText.getText().toString();
+//        intent.putExtra(EXTRA_MESSAGE, message);
+//        startActivity(intent);
+//    }
 
 }
