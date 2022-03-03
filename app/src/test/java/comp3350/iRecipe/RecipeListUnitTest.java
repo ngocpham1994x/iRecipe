3c4dfa879debec8c0697f8f5a7e47c982beb4dd4
bhautiksojitra <48924148+bhautiksojitra@users.noreply.github.com>
2022-03-02 22:35:14 -0600

    Added removeRecipe_tests method in RecipeListUnitTest.java

--- app/src/test/java/comp3350/iRecipe/RecipeListUnitTest.java
+++ app/src/test/java/comp3350/iRecipe/RecipeListUnitTest.java
@@ -40,9 +40,29 @@ public class RecipeListUnitTest {
         assertTrue(recipeList.addRecipe(r1));
 
         assertEquals(recipeList.getAllRecipes().size() , 4);
-
+        assertTrue(recipeList.getAllRecipes().contains(r1));
 
     }
+
+    @Test
+    public void removeRecipeTest()
+    {
+        Recipe r1 = new Recipe("eggs" , "breakfast");
+        Recipe r2 = new Recipe("pancakes" , "dinner");
+
+        // No recipes exists
+        assertFalse("Should return False , r1 does not exists " , recipeList.removeRecipe(r1));
+        recipeList.addRecipe(r1);
+        recipeList.addRecipe(r2);
+        assertTrue("r1 is removable , should return true" , recipeList.removeRecipe(r1));
+        assertEquals(1 , recipeList.getAllRecipes().size());
+
+        assertTrue(recipeList.removeRecipe(r2));
+
+        assertEquals(0 , recipeList.getAllRecipes().size() );
+
+    }
+
 
 
 }
