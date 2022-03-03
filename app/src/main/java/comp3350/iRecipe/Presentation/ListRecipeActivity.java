2398faaa9a8e9a8a4e91dc8bdcfa13303afb87d6
Yixi <wuy8@myumanitoba.ca>
2022-03-03 12:32:29 -0600

    Merge all 5 activities that list recipe into one

--- app/src/main/java/comp3350/iRecipe/Presentation/ListRecipeActivity.java
+++ app/src/main/java/comp3350/iRecipe/Presentation/ListRecipeActivity.java
@@ -4,10 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
+import comp3350.iRecipe.Business.SearchRecipe;
 import comp3350.iRecipe.Persistence.GetRecipeFromCSV;
 import comp3350.iRecipe.Persistence.RecipeList;
 import comp3350.iRecipe.R;
@@ -28,9 +31,24 @@ public class ListRecipeActivity extends AppCompatActivity {
         recyclerView_recipe.setLayoutManager(new LinearLayoutManager(this));
         recyclerView_recipe.setHasFixedSize(true);
 
-
         list = new RecipeList(this);
-        ArrayList<Recipe> recipe_list = list.getAllRecipes();
+        ArrayList<Recipe> recipe_list;
+        ArrayList<Recipe> allRecipe = list.getAllRecipes();
+
+        Intent intent = getIntent();
+        String category = intent.getStringExtra("type");
+
+        TextView tv = (TextView)findViewById(R.id.titleText);
+
+
+        if(category.equalsIgnoreCase("All")){
+            recipe_list = allRecipe;
+            tv.setText("All Recipes");
+        }else{
+            SearchRecipe searching = new SearchRecipe(list);
+            recipe_list = searching.getRecipesByCategory(category);
+            tv.setText("All " + category);
+        }
 
         AdapterRecipe adapter_recipe = new AdapterRecipe(recipe_list);
         recyclerView_recipe.setAdapter(adapter_recipe);
