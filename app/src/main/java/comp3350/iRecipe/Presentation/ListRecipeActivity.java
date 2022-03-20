12ad62bdf2dbc44738d3d98bcbf9ab9a7c0afc89
NgocPham <ngoc.pham.nm@gmail.com>
2022-03-20 03:53:00 -0500

    fix margin activity_list_recipe.xml, added search_by_dropdown.xml, added search codes in MainActivity.java and ListRecipeActivity.java

--- app/src/main/java/comp3350/iRecipe/Presentation/ListRecipeActivity.java
+++ app/src/main/java/comp3350/iRecipe/Presentation/ListRecipeActivity.java
@@ -33,26 +33,50 @@ public class ListRecipeActivity extends AppCompatActivity {
         recyclerView_recipe.setHasFixedSize(true);
 
         list = new RecipeListHSQLDB(MainActivity.getDBPathName());
-        ArrayList<Recipe> recipe_list;
+        ArrayList<Recipe> recipe_list = null;
         ArrayList<Recipe> allRecipe = list.getAllRecipes();
 
         Intent intent = getIntent();
         String category = intent.getStringExtra("type");
+        String recipeName = intent.getStringExtra("recipeName");
 
         TextView tv = (TextView)findViewById(R.id.titleText);
 
+
+        if(category != null){
+            recipe_list = showRecipeByCategory(category, allRecipe, tv);
+        }
+        else if (recipeName != null)
+        {
+            recipe_list = showRecipeBySearchResult(recipeName,allRecipe, tv);
+        }
+
+        AdapterRecipe adapter_recipe = new AdapterRecipe(recipe_list , this);
+        recyclerView_recipe.setAdapter(adapter_recipe);
+    }
+
+    public ArrayList<Recipe> showRecipeByCategory(String category, ArrayList<Recipe> allRecipe, TextView tv)
+    {
+        ArrayList<Recipe> recipe_list;
 
         if(category.equalsIgnoreCase("All")){
             recipe_list = allRecipe;
             tv.setText("All Recipes");
         }else{
-
             recipe_list = SearchRecipe.getRecipesByCategory(category,allRecipe);
             tv.setText("All " + category);
         }
 
-        AdapterRecipe adapter_recipe = new AdapterRecipe(recipe_list , this);
-        recyclerView_recipe.setAdapter(adapter_recipe);
+        return recipe_list;
+    }
 
+    public ArrayList<Recipe> showRecipeBySearchResult(String recipeName, ArrayList<Recipe> allRecipe, TextView tv)
+    {
+        ArrayList<Recipe> recipe_list;
+
+        recipe_list = SearchRecipe.matchByName(recipeName, allRecipe);
+        tv.setText("Search result by Recipe Name: \"" + recipeName + "\"");
+
+        return recipe_list;
     }
 }
