5b3ca1ec9bc057f6b1b7988b8f7a9c65c908c4b0
Bhautik Sojitra <sojitrbm@myumanitoba.ca>
2022-03-30 10:39:16 -0500

    added CommentIT in IntegrationTests.java

--- app/src/test/java/comp3350/iRecipe/IntegrationTests.java
+++ app/src/test/java/comp3350/iRecipe/IntegrationTests.java
@@ -5,13 +5,15 @@ import org.junit.runners.Suite;
 
 import comp3350.iRecipe.Business.SearchRecipeTest;
 import comp3350.iRecipe.Objects.RecipeTest;
+import comp3350.iRecipe.Persistence.CommentIT;
 import comp3350.iRecipe.Persistence.RecipeListIT;
 import comp3350.iRecipe.Persistence.RecipeListUnitTest;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        RecipeListIT.class
+        RecipeListIT.class,
+        CommentIT.class
 })
 
 public class IntegrationTests {
