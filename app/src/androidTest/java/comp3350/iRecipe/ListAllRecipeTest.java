666b6d915e2cff1f53c7523992059f12e06f756b
Bhautik Sojitra <sojitrbm@myumanitoba.ca>
2022-04-11 18:54:50 -0500

    fixed ListAllRecipeTest

--- app/src/androidTest/java/comp3350/iRecipe/ListAllRecipeTest.java
+++ app/src/androidTest/java/comp3350/iRecipe/ListAllRecipeTest.java
@@ -1,14 +1,19 @@
 package comp3350.iRecipe;
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+
 import android.support.test.filters.LargeTest;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
+
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
 import comp3350.iRecipe.Presentation.MainActivity;
-import static android.support.test.espresso.Espresso.onView;
-import static android.support.test.espresso.action.ViewActions.click;
-import static android.support.test.espresso.matcher.ViewMatchers.withText;
-import android.support.test.rule.ActivityTestRule;
 
 @RunWith(AndroidJUnit4.class)
 @LargeTest
