77e339ccdf20dc96c107dbd2478e068155cf4a81
Yixi <wuy8@myumanitoba.ca>
2022-04-19 14:44:26 -0500

    Write the mockito test

--- app/src/test/java/comp3350/iRecipe/Persistence/CommentMockito.java
+++ app/src/test/java/comp3350/iRecipe/Persistence/CommentMockito.java
@@ -3,8 +3,12 @@ package comp3350.iRecipe.Persistence;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import comp3350.iRecipe.Objects.Comment;
 import comp3350.iRecipe.Objects.Recipe;
@@ -12,23 +16,46 @@ import comp3350.iRecipe.Objects.Recipe;
 
 public class CommentMockito {
 
-    CommentHSQLDB comments = mock(CommentHSQLDB.class);
-    RecipeListHSQLDB recipeList = mock(RecipeListHSQLDB.class);
+    RecipeCommentInterface commentGetter;
+    RecipeListInterface recipeGetter;
+
+    @Before
+    public void setup(){
+        commentGetter = mock(RecipeCommentInterface.class);
+        recipeGetter = mock(RecipeListInterface.class);
+    }
 
     @Test
     public void integrationTest()
     {
-        Recipe r1 = new Recipe("Recipe1" , "Main dishes");
+        String recipeName = "Recipe1";
+        ArrayList<Recipe> recipeList = new ArrayList<>();
 
-        recipeList.addRecipe(r1 );
-        verify(recipeList).addRecipe(r1);
-        Comment c1 = new Comment(5 , "Nice one ! " , "user 1");
-        assertTrue(comments.addComments("Recipe1" , c1));
-        verify(comments).addComments(r1.getName(), c1);
-        //System.out.println(comments.getComments(r1.getName()).get(0).getUserName());
+        //Test for the recipe
+        Recipe r1;
+        recipeList.add(new Recipe(recipeName , "Main dishes"));
+        when(recipeGetter.searchByName(recipeName)).thenReturn(recipeList.get(0));
 
+        r1 = recipeGetter.searchByName(recipeName);
+        assertTrue(recipeName.equals(r1.getName()));
 
+        verify(recipeGetter).searchByName(recipeName);
 
+        String userName = "Tester";
+        int rating = 5;
+        String myComment = "Great";
+        Comment comment;
+        ArrayList<Comment> comments = new ArrayList<>();
+        comments.add(new Comment(5,"Great","Tester"));
+        when(commentGetter.getComments(recipeName)).thenReturn(comments);
+
+        comment = commentGetter.getComments(recipeName).get(0);
+
+        assertTrue(comment.getUserName().equals(userName));
+        assertTrue(comment.getRating() == rating);
+        assertTrue(comment.getComment().equals(myComment));
+
+        verify(commentGetter).getComments(recipeName);
     }
 
 
